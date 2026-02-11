6a629fefc7ee25955a9340f1d783c3585b44a293
founder786 <indiasrk80@gmail.com>
2026-02-11 14:14:01 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🔥 Ludo Public Server Live 🔥");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
+
